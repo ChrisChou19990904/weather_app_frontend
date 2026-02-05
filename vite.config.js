33f28128e572b2200673a0480fbd69c246dcfea7
Chris Chou <D1397102@o365.fcu.edu.tw>
2026-02-05 15:24:44 +0800

    asd

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,21 @@
 import { fileURLToPath, URL } from 'node:url'
-
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
+  // 🌟 必須加入這行，對齊你的 GitHub 專案名稱
+  base: '/weather_app/',
   plugins: [vue()],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
+  },
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:8080', // 轉發到你的 Java 後端
+        target: 'http://localhost:8080',
         changeOrigin: true
       }
     }
